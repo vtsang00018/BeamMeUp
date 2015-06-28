665011a37ec2c883eec7950a07c44086c55910d1
Vincent <Vincent.Tsang@tufts.edu>
2015-06-28 12:37:30 -0700

    Leveraged Loaders, Loader projections, and made Details View Functional

--- app/src/main/java/com/example/vincent/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/vincent/sunshine/DetailActivity.java
@@ -1,19 +1,11 @@
 package com.example.vincent.sunshine;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.widget.ShareActionProvider;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 
 public class DetailActivity extends ActionBarActivity {
-
-    private ShareActionProvider mShareActionProvider;
-    private final String sunshineHash = " #Sunshine";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,58 +13,4 @@ public class DetailActivity extends ActionBarActivity {
         setContentView(R.layout.activity_detail);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_detail, menu);
-
-        // Locate MenuItem with ShareActionProvider
-        MenuItem item = menu.findItem(R.id.menu_item_share);
-
-        // Fetch and store ShareActionProvider
-        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
-
-        // attach intent to the share provider
-        setShareIntent(getForecastIntent());
-
-        return true;
-    }
-    private Intent getForecastIntent(){
-
-        // get the intent string containing the forecast
-        String forecastStr = getIntent().getStringExtra(Intent.EXTRA_TEXT);
-        // create new intent
-        Intent shareIntent = new Intent();
-        // prevent activity called from being placed on activity stack
-        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
-
-        shareIntent.setAction(Intent.ACTION_SEND);
-        shareIntent.putExtra(Intent.EXTRA_TEXT, forecastStr + sunshineHash);
-        shareIntent.setType("text/plain");
-        return shareIntent;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        switch(id) {
-            //noinspection SimplifiableIfStatement
-            case R.id.action_settings:
-                Intent settingsIntent = new Intent(DetailActivity.this, SettingsActivity.class);
-                startActivity(settingsIntent);
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    // Call to update the share intent
-    private void setShareIntent(Intent shareIntent) {
-        if (mShareActionProvider != null) {
-            mShareActionProvider.setShareIntent(shareIntent);
-        }
-    }
 }
--- app/src/main/java/com/example/vincent/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/vincent/sunshine/ForecastFragment.java
@@ -1,12 +1,13 @@
 package com.example.vincent.sunshine;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
-import android.util.Log;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -14,67 +15,62 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.util.ArrayList;
+import com.example.vincent.sunshine.data.WeatherContract;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class ForecastFragment extends Fragment {
+/**
+ * Encapsulates fetching the forecast and displaying it as a {@link ListView} layout.
+ */
+/**
+ * Encapsulates fetching the forecast and displaying it as a {@link ListView} layout.
+ */
+public class ForecastFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
-    // tag for logcat
-    private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
-    ArrayAdapter<String> listAdapter;
-    ListView mListView;
-    String zipCode;
-    boolean isImperial;
+    private static final int FORECAST_LOADER = 0;
+    private ForecastAdapter mForecastAdapter;
+
+    private static final String[] FORECAST_COLUMNS = {
+            // In this case the id needs to be fully qualified with a table name, since
+            // the content provider joins the location & weather tables in the background
+            // (both have an _id column)
+            // On the one hand, that's annoying.  On the other, you can search the weather table
+            // using the location set by the user, which is only in the Location table.
+            // So the convenience is worth it.
+            WeatherContract.WeatherEntry.TABLE_NAME + "." + WeatherContract.WeatherEntry._ID,
+            WeatherContract.WeatherEntry.COLUMN_DATE,
+            WeatherContract.WeatherEntry.COLUMN_SHORT_DESC,
+            WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
+            WeatherContract.WeatherEntry.COLUMN_MIN_TEMP,
+            WeatherContract.LocationEntry.COLUMN_LOCATION_SETTING,
+            WeatherContract.WeatherEntry.COLUMN_WEATHER_ID,
+            WeatherContract.LocationEntry.COLUMN_COORD_LAT,
+            WeatherContract.LocationEntry.COLUMN_COORD_LONG
+    };
+
+    // These indices are tied to FORECAST_COLUMNS.  If FORECAST_COLUMNS changes, these
+    // must change.
+    static final int COL_WEATHER_ID = 0;
+    static final int COL_WEATHER_DATE = 1;
+    static final int COL_WEATHER_DESC = 2;
+    static final int COL_WEATHER_MAX_TEMP = 3;
+    static final int COL_WEATHER_MIN_TEMP = 4;
+    static final int COL_LOCATION_SETTING = 5;
+    static final int COL_WEATHER_CONDITION_ID = 6;
+    static final int COL_COORD_LAT = 7;
+    static final int COL_COORD_LONG = 8;
 
     public ForecastFragment() {
-    }
-
-    private double convertToFahrenheit(double temp){
-        double fTemp = temp * (9/5) + 32;
-        return fTemp;
-    }
-
-    private void updateWeather(){
-        FetchWeatherTask refresh = new FetchWeatherTask(getActivity(), listAdapter);
-
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
-
-        String defaultVal = getString(R.string.pref_location_default);
-        String defaultTemp = getResources().getString(R.string.pref_temp_default);
-
-        zipCode = sharedPreferences.getString(getString(R.string.pref_location_key), defaultVal);
-        isImperial = sharedPreferences.getString(getString(R.string.pref_temp_key), defaultTemp)
-                .equalsIgnoreCase(defaultTemp);
-        refresh.execute(zipCode);
-    }
-
-    private void viewMap(Uri geoLocation){
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(geoLocation);
-        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
-            startActivity(intent);
-        } else {
-          Log.d(LOG_TAG, "Couldn't resolve intent");
-        }
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        // update weather so that the new weather is displayed even if settings are modified
-        updateWeather();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // declares that a menu exists and allows for handling menu options
+        // Add this line in order for this fragment to handle menu events.
         setHasOptionsMenu(true);
     }
 
@@ -85,23 +81,13 @@ public class ForecastFragment extends Fragment {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        switch(id) {
-            // if settings is clicked
-            case R.id.action_settings:
-                Intent settingsIntent = new Intent(getActivity(), SettingsActivity.class);
-                startActivity(settingsIntent);
-                break;
-            // if refresh is clicked
-            case R.id.action_refresh:
-                updateWeather();
-                return true;
-
-            case R.id.action_location:
-                String uri = "geo:0,0?q=" + zipCode;
-                Uri geoCode = Uri.parse(uri);
-                viewMap(geoCode);
-
+        if (id == R.id.action_refresh) {
+            updateWeather();
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
@@ -109,37 +95,78 @@ public class ForecastFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // declare the listview adapter for the forecast data
+        // The CursorAdapter will take data from our cursor and populate the ListView.
+        mForecastAdapter = new ForecastAdapter(getActivity(), null, 0);
+
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        // find the ListView for the forecasts
-        mListView = (ListView)rootView.findViewById(R.id.listview_forecast);
+        // Get a reference to the ListView, and attach this adapter to it.
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mForecastAdapter);
 
-        // create the list adapter for the forecasts
-        listAdapter = new ArrayAdapter<String>(
-                                // the current context (parent activity)
-                                getActivity(),
-                                // the layout ID to modify
-                                R.layout.list_item_forecast,
-                                // the view ID that will be accessed
-                                R.id.list_item_forecast_textview,
-                                // the forecast data that will be used
-                                new ArrayList<String>());
+        // We'll call our MainActivity
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
-        // bind the adapter to the listview
-        mListView.setAdapter(listAdapter);
-
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String forecast = listAdapter.getItem(position).toString();
-                Intent detailIntent = new Intent(getActivity(), DetailActivity.class)
-                        .putExtra(Intent.EXTRA_TEXT, forecast );
-                startActivity(detailIntent);
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // CursorAdapter returns a cursor at the correct position for getItem(), or null
+                // if it cannot seek to that position.
+                Cursor cursor = (Cursor) adapterView.getItemAtPosition(position);
+                if (cursor != null) {
+                    String locationSetting = Utility.getPreferredLocation(getActivity());
+                    Intent intent = new Intent(getActivity(), DetailActivity.class)
+                            .setData(WeatherContract.WeatherEntry.buildWeatherLocationWithDate(
+                                    locationSetting, cursor.getLong(COL_WEATHER_DATE)
+                            ));
+                    startActivity(intent);
+                }
             }
         });
-
         return rootView;
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        getLoaderManager().initLoader(FORECAST_LOADER, null, this);
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    private void updateWeather() {
+        FetchWeatherTask weatherTask = new FetchWeatherTask(getActivity());
+        String location = Utility.getPreferredLocation(getActivity());
+        weatherTask.execute(location);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateWeather();
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        String locationSetting = Utility.getPreferredLocation(getActivity());
+
+        // Sort order:  Ascending, by date.
+        String sortOrder = WeatherContract.WeatherEntry.COLUMN_DATE + " ASC";
+        Uri weatherForLocationUri = WeatherContract.WeatherEntry.buildWeatherLocationWithStartDate(
+                locationSetting, System.currentTimeMillis());
+
+        return new CursorLoader(getActivity(),
+                weatherForLocationUri,
+                FORECAST_COLUMNS,
+                null,
+                null,
+                sortOrder);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+        mForecastAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> cursorLoader) {
+        mForecastAdapter.swapCursor(null);
+    }
 }
