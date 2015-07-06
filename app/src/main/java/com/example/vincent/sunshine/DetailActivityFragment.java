76c9b306b8335fe2bc87290496a1db9c4ad21e36
Vincent <Vincent.Tsang@tufts.edu>
2015-07-06 15:02:25 -0700

    Added Two-Pane tablet view

--- app/src/main/java/com/example/vincent/sunshine/DetailActivityFragment.java
+++ app/src/main/java/com/example/vincent/sunshine/DetailActivityFragment.java
@@ -149,7 +149,7 @@ public class DetailActivityFragment extends Fragment implements LoaderManager.Lo
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
         Log.v(LOG_TAG, "In onCreateLoader");
         Intent intent = getActivity().getIntent();
-        if (intent == null) {
+        if (intent == null || intent.getData() == null) {
             return null;
         }
 
